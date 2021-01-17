1b25ad68ffe2a62c8317098f36ed6c66356bb9ff
Oğuzhan ÇART <54273323+oguzhan18@users.noreply.github.com>
2021-01-18 00:55:47 +0300

    Add files via upload

---
+++ veri_cekme/veri_cekme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace veri_cekme
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string okunan;
+            StreamReader st = File.OpenText(@"C:\Program Files (x86)\yazi.txt");
+            while ((okunan = st.ReadLine()) != null)
+            {
+                Console.WriteLine(okunan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
